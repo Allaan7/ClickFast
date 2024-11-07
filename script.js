74a8e847f9357944395513695cde86afae9d5d27
Allaan7 <allaan.salim@gmail.com>
2024-11-07 16:12:04 +0100

    Hide audio player

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+import { log } from "console";
+let audioPlayer = document.getElementById("audioPlayer");
 let count = 0;
 
 document.getElementById("button-clicker")
     .addEventListener("click", () => {
         count++;
         document.getElementById("counter").innerHTML = count;
+        console.log(count);
 })
+
+
+audioPlayer.play();
+
+
